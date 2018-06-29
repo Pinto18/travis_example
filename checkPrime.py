e48ead048f55162d4cfd771633b97c061cbb9443
Pinto18 <pinto18.nd@gmail.com>
2018-06-29 10:23:31 -0400

    Created New isPrime function

--- checkPrime.py
+++ checkPrime.py
@@ -1,12 +1,31 @@
 import math
 
-def check(num):
-    isPrime = False
-    if num <= 1:
-        return isPrime
-    for index in range(2, math.ceil(math.sqrt(num))):
-        isPrime = True
-        if num % index == 0:
-            isPrime = False
-            return isPrime
-    return isPrime
+# def check(num):
+    # isPrime = False
+    # if num <= 1:
+        # return isPrime
+    # for index in range(2, math.ceil(math.sqrt(num))):
+        # isPrime = True
+        # if num % index == 0:
+            # isPrime = False
+            # return isPrime
+    # return isPrime
+
+def isPrime(num):
+    if num < 1:
+        return False
+    for index in range(2, num - 1):
+        if num % index == 0: 
+            return False 
+    return True 
+
+def main():
+    number = int(input("Enter a number to see if it is prime: "))
+    if isPrime(number):
+        print(number , " is prime.\n")
+    else:
+        print(number, "is not prime.\n")
+
+
+if __name__=='__main__':
+    main()
--- test_prime.py
+++ test_prime.py
@@ -1,16 +1,23 @@
 import unittest
-from checkPrime import check
+from checkPrime import isPrime 
 
 DATA_TABLE =[
-    {'input': 7, 'expected': True},
+    {'input': -1, 'expected': False},
+    {'input': 3, 'expected': True},
     {'input': 10, 'expected': False}
 ]
 
 class TestPrime(unittest.TestCase):
     def test_check(self):
+        actual = False
         for data in DATA_TABLE:
-            result = check(data['input'])
-            self.assertEqual(result, data['expected'])
+            try:
+                actual = isPrime(data['input'])
+                self.assertEqual(actual, data['expected'])
+            except AssertionError as error:
+                print("Input was ", data['input'], "\nExepected value: ",
+                      data['expected'], "\nActual Value: ", actual)
+                raise Exception(error)
 
 if __name__ == '__main__':
     unittest.main()
